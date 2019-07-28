8da3b8f4cc1d4e883a11f744884b03c555e4448c
Jeremy <jeremyash13@gmail.com>
2019-07-28 11:55:36 -0600

    Update PlaceRating.js
    Updated PlaceRating.js to properly render the rating as stars

--- client/src/components/PlaceRating.js
+++ client/src/components/PlaceRating.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-const starRating = (starCount) => {
-    
-    for (let i = 0; i < starCount; i++) {
-        return <FontAwesomeIcon icon={faStar} />
-    }
-
-}
+const starRating = starCount => {
+  let starArray = [];
+  starCount = Math.round(starCount)
+  for (let i = 0; i < starCount; i++) {
+      starArray = [
+          ...starArray,
+          <FontAwesomeIcon icon={faStar} color="yellow" key={i}/>
+        ];
+  }
+  return starArray;
+};
 export default function PlaceRating(props) {
-    // convert me to Pure Component to see if that renders me only once vs three times on app load
+  // convert me to Pure Component to see if that renders me only once vs three times on app load
 
-    return (
-        <div>
-            {/* <p>{`Rating: ${props.value}`}</p> */}
-            {/* <p>{`Rating: ${starRating(props.value)}`}</p> */}
-            {starRating(props.value)}
-        </div>
-    )
+  return (
+    <div>
+      <p>Rating:{starRating(props.value)}</p>
+    </div>
+  );
 }
