f9f6ce08853c79e0859ea6e6354406c8058fff5c
Jeremy <jeremyash13@gmail.com>
2019-07-28 12:02:53 -0600

    Update PlacePrice.js
    updated PlacePrice.js to render price rating as dollar sign svgs

--- client/src/components/PlacePrice.js
+++ client/src/components/PlacePrice.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
+const priceRating = dollarCount => {
+  let dollarArray = [];
+  dollarCount = Math.round(dollarCount);
+  for (let i = 0; i < dollarCount; i++) {
+    dollarArray = [
+      ...dollarArray,
+      <FontAwesomeIcon icon={faDollarSign} color="green" key={i} />
+    ];
+  }
+  return dollarArray;
+};
 
 export default function PlacePrice(props) {
-    return (
-        <div>
-            <p>{`Price: ${props.value}`}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>Price:{priceRating(props.value)}</p>
+    </div>
+  );
 }
