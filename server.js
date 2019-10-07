92afd5cb5189f1360175b6a81161dba60dbaf6a7
Jeremy Ashcraft <jeremyash13@gmail.com>
2019-10-06 19:35:17 -0600

    preparing server side code for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,5 +19,12 @@ app.post("/", async (req, res) => {
   const json = await data.json();
   res.send(json);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
