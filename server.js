e8c01d56f80cd272076b2b89ce75318e78121e1c
Jeremy Ashcraft <jeremyash13@gmail.com>
2019-10-07 18:12:25 -0600

    changed server.js to pull the port and api key from environment variables

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
-const port = process.env.PORT || 8080;
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +15,7 @@ app.use(bodyParser.json());
 
 app.post("/", async (req, res) => {
   const { location, radius, type } = req.body;
-  const key = 'AIzaSyCAkiTbJB7LAyQx3lBt-P0XYIgZqe5G7Zs';
+  const key = process.env.GOOGLE_API_KEY;
 
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${key}&location=${location}&radius=${radius}&type=${type}`;
 
@@ -27,4 +31,8 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+app.listen(port, () => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`Listening on http://localhost:${port}`);
+  }
+});
