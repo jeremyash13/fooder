2a3ee46670340817012d8f2d5b9b56b5c9ecc059
Jeremy Ashcraft <jeremyash13@gmail.com>
2020-02-03 01:11:01 -0700

    neumorphic stylings applied

--- client/src/components/PlaceTags.js
+++ client/src/components/PlaceTags.js
@@ -8,18 +8,22 @@ import {
   faMusic,
 } from "@fortawesome/free-solid-svg-icons";
 
+const styleTags = {
+  backgroundColor: '#E0E5EC',
+}
+
 export default function PlaceTags(props) {
   const chipTags = tagCount => {
     let chipArray = [];
     for (let i = 0; i < tagCount.length; i++) {
       if (props.value[i] === 'bar') {
-        chipArray = [...chipArray,<Chip label={'Bar'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faGlassCheers} />}/>]
+        chipArray = [...chipArray,<Chip style={styleTags} label={'Bar'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faGlassCheers} />}/>]
       } else if (props.value[i] === 'restaurant') {
-        chipArray = [...chipArray, <Chip label={'Restaurant'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faUtensils} />}/>]
+        chipArray = [...chipArray, <Chip style={styleTags} label={'Restaurant'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faUtensils} />}/>]
       } else if (props.value[i] === 'food') {
-        chipArray = [...chipArray, <Chip label={'Food'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faHamburger} />}/>]
+        chipArray = [...chipArray, <Chip style={styleTags} label={'Food'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faHamburger} />}/>]
       } else if (props.value[i] === 'night_club') {
-        chipArray = [...chipArray, <Chip label={'Night Club'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faMusic} />}/>]
+        chipArray = [...chipArray, <Chip style={styleTags} label={'Night Club'} className="tags--chip" key={i} icon={<FontAwesomeIcon icon={faMusic} />}/>]
       }
     }
     return chipArray;
